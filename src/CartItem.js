693ad79cf51960a574a7a7e26f58c2efdc254433
shobanraj N V <shoban.games@gmail.com>
2022-10-03 14:09:53 +0530

    Add increase quantity

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,45 +1,61 @@
 import React from "react";
 
 class CartItem extends React.Component {
-   constructor () {
-      super ();
-      this.state = {
-         price: 999,
-         title: 'Phone',
-         qty: 1,
-         img: ''
-      }
-   }
-    render () {
-      const {price,title,qty} = this.state;
-       return (
-        <div className="cart-item">
-           <div className="left-block">
-              <img style={styles.image}/>
-            </div> 
-            <div lassName="right-block">
-               <div style={ {fontSize: 25} }>{title}</div>
-               <div style={ {color: '#777'} }>Rs {price}</div>
-               <div style={ {color: '#777'} }>Qty: {qty}</div>
-               <div className="cart-item-actions">
-                  {/* Buttons  */}
-                  <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png" />
-                  <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" />
-                  <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" />
-               </div>
-            </div>
+  constructor() {
+    super();
+    this.state = {
+      price: 999,
+      title: "Phone",
+      qty: 1,
+      img: "",
+    };
+  }
+  increaseQuantity = () => {
+    console.log("this", this.state);
+  };
+  render() {
+    const { price, title, qty } = this.state;
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img style={styles.image} />
         </div>
-       );
-    }
+        <div lassName="right-block">
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: "#777" }}>Rs {price}</div>
+          <div style={{ color: "#777" }}>Qty: {qty}</div>
+          <div className="cart-item-actions">
+            {/* Buttons  */}
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+              onClick={this.increaseQuantity}
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-const styles ={
-    image: {
-        height: 110,
-        width: 110,
-        borderRadius: 4,
-        background: '#ccc'
-    }
-}
+const styles = {
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: "#ccc",
+  },
+};
 
 export default CartItem;
